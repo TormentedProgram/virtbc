e0d0ef0313d9f9e4b208a0e5383491a8b702e85b
Tormented <69302186+TormentedProgram@users.noreply.github.com>
2024-05-31 03:18:03 +1000

    make pip uninstall better

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
+def find_files(directory):
+    files = []
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            files.append(os.path.relpath(os.path.join(root, filename), directory))
+    return files
 
 setup(
     name='virtbc',
-    version='0.1.0',
+    version='0.1.1',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'virtbc = virtbc:main',
         ],
+    },
+    package_data={
+        'virtbc': find_files(os.path.join(os.path.expanduser("~"), '.config', 'virtbc')),
     },
 )
