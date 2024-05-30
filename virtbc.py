dcd227cf6b98ab01c0ea2c6a1546406f8ec162ae
Tormented <69302186+TormentedProgram@users.noreply.github.com>
2024-05-30 15:51:38 +1000

    Update virtbc.py

--- virtbc.py
+++ virtbc.py
@@ -1,6 +1,14 @@
 import os
 import sys
 
+modFolder = os.path.join(os.path.expanduser("~"),'.config','virtbc')
+modManager = os.path.join(modFolder, "virtbc_commands")
+
+def setup_bashrc_mod():
+    os.makedirs(modFolder, exist_ok=True)
+    with open(modManager, 'a'):
+        pass
+
 def find_bashrc():
     # Default paths where .bashrc is commonly located
     possible_paths = [
@@ -13,39 +21,59 @@ def find_bashrc():
 
     return None
 
-def add_function_to_bashrc(function_name):  
+def add_manager_to_bashrc():  
+    setup_bashrc_mod()
     bashrc_path = find_bashrc()
 
     if bashrc_path:
-        venv_path = os.path.join(os.path.expanduser("~"), ".venvs", "globalvenv", "bin", "activate")
-        function_code = """
-{}() {{
-    source {venv_path}
-    local function_name="${{FUNCNAME[0]}}"
-    local command_name="$(which "$function_name")"
-    if [ -z "$command_name" ]; then
-        echo "Command not found!"
-        return 1
-    fi
-    "$command_name" "$@"
-    deactivate
-}}
-        """.format(function_name)
+        function_code = f'''
+#virtbc implementation
+source {modManager}
+        '''
 
         try:
             with open(bashrc_path, 'a') as bashrc_file:
                 bashrc_file.write(function_code)
-            print("Function successfully added to .bashrc file:", bashrc_path)
-            os.system("exec bash -l")
-            print(".bashrc refreshed successfully.")
         except Exception as e:
             print("Error:", e)
     else:
         print("Could not locate .bashrc file.")
+
+def add_function(function_name):  
+    currentMod = os.path.join(modFolder, function_name)
+    bashrc_path = find_bashrc()
+
+    if currentMod:
+        vert_path = os.path.join(os.path.expanduser("~"),'.venvs','globalvenv','bin','activate')
+        function_code = f'''
+{function_name}() {{
+    source {vert_path}
+    local function_name="${{FUNCNAME[0]}}"
+    local command_name="$(which "$function_name")"
+    "$command_name" "$@"
+    deactivate
+}}
+        '''
+
+        try:
+            with open(currentMod, 'a') as bash_mod:
+                bash_mod.write(function_code)
+
+            with open(modManager, 'a') as bashrc_file:
+                bashrc_file.write(f'''
+source {currentMod}
+''')
+
+            print("Function successfully added to .bashrc file:", currentMod)
+            os.system("exec bash -l")
+            print(".bashrc refreshed successfully.")
+        except Exception as e:
+            print("Error:", e)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: virtbc function_name")
     else:
         function_name = sys.argv[1]
-        add_function_to_bashrc(function_name)
+        add_manager_to_bashrc()
+        add_function(function_name)
